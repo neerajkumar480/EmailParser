44ad883b5e26172532b009f47bd19ef3c665481b
D Neeraj Kumar <d.neerajkumar480@gmail.com>
2024-11-25 23:54:28 +0530

    intial commit with only health check stats

---
+++ src/test/java/com/volcanogroup/EmailParser/EmailParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volcanogroup.EmailParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
